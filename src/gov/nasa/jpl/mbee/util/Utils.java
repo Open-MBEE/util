7d1e2bd1da0630a0eefe70500d4d90139ab79d60
Gabriel Harris-Rouquette <gabizou@me.com>
2014-11-08 14:22:45 -0800

    Convert project to use Gradle instead of Maven. All adapted features provided by the Maven configuration have been reproduced with Gradle. The new Gradle script also supports automatic artifact signing with GNU Privacy Guard keys. Included is the Gradle Wrapper that can be used by anyone to automatically build the project without having to install Gradle itself.

--- src/gov/nasa/jpl/mbee/util/Utils.java
+++ src/gov/nasa/jpl/mbee/util/Utils.java
@@ -1,5 +1,5 @@
-/*******************************************************************************
- * Copyright (c) <2013>, California Institute of Technology ("Caltech").
+/**
+ * Copyright (c) <2013>, California Institute of Technology ("CalTech").
  * U.S. Government sponsorship acknowledged.
  *
  * All rights reserved.
@@ -25,7 +25,7 @@
  * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- ******************************************************************************/
+ */
 package gov.nasa.jpl.mbee.util;
 
 //import gov.nasa.jpl.ae.event.Expression;
