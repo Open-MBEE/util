9da2c7ac1dea0f74778f9afe48cf27c590c4cc13
Bradley J. Clement <bclement@jpl.nasa.gov>
2018-01-29 15:29:57 -0800

    removed unused import from java 1.8

--- src/gov/nasa/jpl/mbee/util/TimeUtils.java
+++ src/gov/nasa/jpl/mbee/util/TimeUtils.java
@@ -3,7 +3,6 @@ package gov.nasa.jpl.mbee.util;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Duration;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
